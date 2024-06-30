680abd3c65bc823f6ea35a84cc2da59a167c71a8
Pablo Pinto <pablohernanpinto@gmail.com>
2024-06-30 16:47:31 -0400

    se termino front

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,10 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
+
+    implementation(libs.retrofit)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.Manifest
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -13,7 +12,6 @@ import android.telephony.TelephonyManager
 import android.text.format.Formatter
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -23,12 +21,24 @@ import android.util.Log
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+    private val gson = Gson()
     private var phoneNumber: String? = null
     private var ssid: String? = null
     private var ip: String? = null
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private var scannedQRCode:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,15 +53,45 @@ class MainActivity : AppCompatActivity() {
         val btn_scan: Button = findViewById(R.id.btn_scan)
 
 
-
+        //MAIN
         btn_scan.setOnClickListener {
             IntentIntegrator(this).initiateScan()
             displayWifiInfo()
             Log.e("TAG", "SSID: $ssid, IP: $ip")
             phoneNumber = getNumber(it)
             Log.e("TAG", "Phone Number: $phoneNumber")
+            Log.e("TAG", "Qr: $scannedQRCode")
+        }
+        //fetchPruebaData()
 
+    }
 
+    private fun fetchPruebaData() {
+        //aqui se manda la data
+        CoroutineScope(Dispatchers.IO).launch {
+            val request = Request.Builder()
+                .url("http://localhost:3000/prueba")
+                .build()
+
+            client.newCall(request).execute().use { response ->
+                if (response.isSuccessful) {
+                    response.body?.let { responseBody ->
+                        val json = responseBody.string()
+                        val listType = object : TypeToken<List<Prueba>>() {}.type
+                        val pruebaList: List<Prueba> = gson.fromJson(json, listType)
+
+                        withContext(Dispatchers.Main) {
+                            // Aquí puedes actualizar la UI con los datos obtenidos
+                            //findViewById<TextView>(R.id.textView).text = pruebaList.joinToString("\n")
+                        }
+                    }
+                } else {
+                    // Manejar el error de la solicitud
+                    withContext(Dispatchers.Main) {
+                        //findViewById<TextView>(R.id.textView).text = "Error: ${response.code}"
+                    }
+                }
+            }
         }
     }
 
@@ -132,6 +172,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Escaneo cancelado", Toast.LENGTH_LONG).show()
             } else {
                 // El escaneo fue exitoso
+                scannedQRCode = result.contents
+
                 Toast.makeText(this, "Código escaneado: " + result.contents, Toast.LENGTH_LONG).show()
             }
         } else {
@@ -139,4 +181,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
 }
+
+data class Prueba(
+    val SSID: String,
+    val Numero: String,
+    val IP: String
+)
